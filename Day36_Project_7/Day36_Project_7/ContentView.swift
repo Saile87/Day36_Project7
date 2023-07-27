9f6e6d3e1e823887eee6bf7df8a0af796f41b6ae
Elias Breitenbach <elias.b@me.com>
2023-07-27 23:29:07 +0200

    Showing and hiding views

--- Day36_Project_7/Day36_Project_7/ContentView.swift
+++ Day36_Project_7/Day36_Project_7/ContentView.swift
@@ -7,17 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        Text("Hello, \(name)")
+        Button("Dismiss") {
+            dismiss()
         }
-        .padding()
     }
 }
+
+struct ContentView: View {
+    
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show Sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "Elias")
+        }
+    }
+}
+
+//class User: ObservableObject {
+//    @Published var firstName = "Elias"
+//    @Published var lastName = "Breitenbach"
+//}
+//
+//struct ContentView: View {
+//    @StateObject var user = User()
+//
+//    var body: some View {
+//        VStack {
+//            Text("Your name is \(user.firstName) \(user.lastName).")
+//
+//            TextField("First name", text: $user.firstName)
+//            TextField("Last name", text: $user.lastName)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
